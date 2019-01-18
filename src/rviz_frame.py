de7beecd48ee20a2a7854103fb63b27466fe53bc
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-01-17 21:26:46 -0800

    Still in progress

--- src/rviz_frame.py
+++ src/rviz_frame.py
@@ -2,29 +2,67 @@ import roslib
 import rviz
 import rospy
 import rosbag
-import rosnode
 import sys
 
 from sensor_msgs.msg import PointCloud2
 from visualization_msgs.msg import MarkerArray
+from visualization_msgs.msg import Marker
 
 from multiprocessing import Process
 
 from python_qt_binding.QtGui import *
 from python_qt_binding.QtCore import *
 
-def publisher_process(cloud_pub, cloud, marker_pub, markers):
+def readBag(file_path):
+    bag = rosbag.Bag(file_path)
+    cloud = None
+    markers = None
+
+    for topic, msg, t in bag.read_messages():
+        if topic == 'cloud':
+            cloud = msg
+        elif topic == 'markers':
+            markers = msg
+        else:
+            continue
+
+    bag.close()
+
+    return (cloud, markers)
+
+def publisher_process():
     rospy.init_node('rviz_result_form')
     while rospy.Time().now().to_sec() == 0:
         pass
+
+    cloud_pub = rospy.Publisher('cloud', PointCloud2, queue_size=200)
+    marker_pub = rospy.Publisher('markers', MarkerArray, queue_size=200)
+
+    current_bag = None
+    cloud = None
+    markers = MarkerArray()
+
     while not rospy.is_shutdown():
+        if current_bag != rospy.get_param('bag_to_read'):
+            marker_arr = MarkerArray()
+            for marker in markers.markers:
+                marker_to_delete = Marker()
+                marker_to_delete.header = marker.header
+                marker_to_delete.ns = marker.ns
+                marker_to_delete.id = marker.id
+                marker_to_delete.action = marker_to_delete.DELETE
+                marker_arr.markers.append(marker_to_delete)
+            marker_pub.publish(marker_arr)
+
+            current_bag = rospy.get_param('bag_to_read')
+            cloud, markers = readBag(current_bag)
         cloud_pub.publish(cloud)
         marker_pub.publish(markers)
 
     print "Process shutdown as rospy is shutdown"
 
 class RvizFrame(QWidget):
-    def __init__(self):
+    def __init__(self, bag_list=[]):
         QWidget.__init__(self)
         self.frame = rviz.VisualizationFrame()
         self.frame.setSplashPath('')
@@ -38,41 +76,33 @@ class RvizFrame(QWidget):
         self.frame.setStatusBar(None)
         self.frame.setHideButtonVisibility(False)
 
+        self.nextButton = QPushButton('next')
+        self.nextButton.clicked.connect(self.readNextBag)
+
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.frame)
+        self.layout.addWidget(self.nextButton)
+        self.layout.addWidget(QLineEdit())
+        self.layout.addWidget(QLineEdit())
         self.setLayout(self.layout)
 
-        self.cloud_pub = rospy.Publisher('cloud', PointCloud2, queue_size=1)
-        self.marker_pub = rospy.Publisher('markers', MarkerArray, queue_size=1)
+        self.bag_lst = list(bag_list)
+        self.lst_idx = 0
+        rospy.set_param('bag_to_read', self.bag_lst[self.lst_idx])
 
-        self.cloud = None
-        self.markers = None
-
-    def readBag(self, file_path):
-        bag = rosbag.Bag(file_path)
-
-        for topic, msg, t in bag.read_messages():
-            if topic == 'cloud':
-                self.cloud = msg
-            elif topic == 'markers':
-                self.markers = msg
-            else:
-                continue
-
-        bag.close()
+    def readNextBag(self):
+        self.lst_idx += 1
+        self.lst_idx %= len(self.bag_lst)
+        rospy.set_param('bag_to_read', self.bag_lst[self.lst_idx])
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
+    lst = ['saved_exp_result1.bag', 'saved_exp_result2.bag']
 
-    frame = RvizFrame()
+    frame = RvizFrame(lst)
     frame.resize(500, 500)
-    frame.readBag('saved_exp_result.bag')
-    process = Process(target=publisher_process, args=[frame.cloud_pub, frame.cloud, frame.marker_pub, frame.markers])
+    process = Process(target=publisher_process)
     process.start()
     frame.show()
     app.exec_()
-    #sys.exit()
-    #rosnode.kill_node(["rviz_result_form"])
-    #rospy.signal_shutdown("Shutting down rospy as the main process is about to terminate")
     process.terminate()
